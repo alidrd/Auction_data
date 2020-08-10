2448a77111169c7be7468f5761c0a4652707fd54
alidarudi@unibas.ch <666007>
2020-08-10 22:30:35 +0200

    grid search checked plot of the reduced version

--- KNN.py
+++ KNN.py
@@ -6,6 +6,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
+from sklearn import preprocessing
 
 
 country = 'FR'
@@ -22,37 +23,39 @@ for neighbour in neighbours:
 # 2D K nearest neighbours
 fig = go.Figure()
 d_reduced = 10000
-for no_neighbours in [100]:
-    residual_demand_columns = [i for i in country_data.columns if i.endswith('Residual_Demand')]
-    X = country_data.loc[:, residual_demand_columns].values.reshape(-1, len(residual_demand_columns))
-    Y = country_data.Price.values.reshape(-1, 1)
-    # X_Y_value = (X.notnull())
-    # X_nonan = X[X_Y_value]
-    # X_resh = X[X_Y_value].values.reshape(-1, 1)
-    # neigh = KNeighborsRegressor(n_neighbors=no_neighbours, weights='uniform')
-    # neigh.fit(X, Y)
-    neigh = KNeighborsRegressor()  # todo: preprocessing --- plot recuded
-    param_grid = [{'n_neighbors': [2, 5, 6, 10], 'weights': ['uniform', 'distance']}]
-    clf = GridSearchCV(neigh, param_grid, scoring='neg_mean_absolute_error', cv=100, refit=True)
-    clf.fit(X, Y)
-    print(clf.best_params_)
-    print(clf.best_score_)
-    # cross_val_score(neigh, X, Y, cv=5, scoring='neg_root_mean_squared_error')
-    Y_pred = clf.predict(X)
-    mse = mean_squared_error(Y, Y_pred)
-    X_reduced = np.copy(X)
-    X_reduced[:, 0] = X_reduced[:, 0] - d_reduced
-    Y_pred_reduced = clf.predict(X_reduced)
 
-    orders = np.argsort(X[:,0].flatten())
-    X_sorted = X[orders]
-    # X_sorted = np.sort(X[:,0].flatten()).reshape(-1, 1)
-    Y_pred_ordered = clf.predict(X_sorted)
 
-    Y_sorted = Y.flatten()[orders]
+residual_demand_columns = [i for i in country_data.columns if i.endswith('Residual_Demand')]
+X = country_data.loc[:, residual_demand_columns].values.reshape(-1, len(residual_demand_columns))
+Y = country_data.Price.values.reshape(-1, 1)
+# X_scaled = preprocessing.scale(X)
+X_scaled = X
+neigh = KNeighborsRegressor()
+param_grid = [{'n_neighbors': [2, 5, 10, 52], 'weights': ['uniform']}]
+clf = GridSearchCV(neigh, param_grid, scoring='neg_mean_squared_error', cv=100, refit=True)
+clf.fit(X_scaled, Y)
+print(clf.best_params_)
+print(clf.best_score_)
+Y_pred = clf.predict(X_scaled)
+mse = mean_squared_error(Y, Y_pred)
 
-    fig.add_trace(go.Scatter(x=X[:,0].flatten(), y=Y.flatten(), mode='markers', name='Original'))
-    fig.add_trace(go.Scatter(x=X_sorted[:,0].flatten(), y=Y_pred_ordered.flatten(), mode='lines',name='Fit:' + str(clf.best_params_['n_neighbors']) + ' RMSE: ' + str(clf.best_score_)))
-#     fig.add_trace(go.Scatter(x=np.sort(X_nonan.values-d_reduced).reshape(-1, 1).flatten(), y=Y_pred_reduced.flatten(), mode='lines', name='Fit reduced'))
-# fig.show()
+orders = np.argsort(X[:,0].flatten())
+X_sorted = X_scaled[orders]
+Y_pred_ordered = clf.predict(X_sorted)
+
+X_reduced = np.copy(X)
+X_reduced[:, 0] = X_reduced[:, 0] - d_reduced
+Y_pred_reduced = clf.predict(X_reduced)
+Y_pred_reduced = Y_pred_reduced[orders]
+
+fig.add_trace(go.Scatter(x=X_scaled[:, 0].flatten(), y=Y.flatten(), mode='markers', name='Original'))
+fig.add_trace(go.Scatter(x=X_sorted[:, 0].flatten(), y=Y_pred_ordered.flatten(), mode='lines', name='Fit:' + str(clf.best_params_['n_neighbors']) + ' RMSE: ' + str(clf.best_score_)))
+fig.add_trace(go.Scatter(x=X_sorted[:, 0].flatten(), y=Y_pred_reduced.flatten(), mode='lines', name='Fit:' + str(clf.best_params_['n_neighbors']) + ' Reduced' ))
+unique, counts = np.unique((Y_pred_ordered - Y_pred_reduced)>0, return_counts=True)
+a = dict(zip(unique, counts))
+fig.update_layout(
+    title="Number of hours in which the price is not reduced: " + str(a[False]) + ' i.e. % ' + str(100 * a[False]/(a[False]+a[True]))
+)
+fig.show()
+print()
 fig.write_html("figures/" + country + "_KKN.html")
